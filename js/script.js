cbc0b9fe4a94a486aed16aeb63fd3a5687045ef5
unknown <juanjoservin2@hotmail.com>
2022-06-07 00:13:44 -0400

    Jugando con manejo del doom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //  querySelector
-
+/*
 const heading = document.querySelector('.header__texto h2'); // 0 o 1 Elementos
 heading.textContent = "Nuevo Heading";
 heading.classList.add('nueva-clase');
@@ -8,4 +8,33 @@ console.log(heading);
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = "Nuevo texto para enlace";
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
+console.log(enlaces[0]);
+
 //getElementById
+
+const heading2 = document.getElementById('heading');
+*/
+
+// Generar un nuevo enlace
+
+const nuevoEnlace = document.createElement('A');
+
+//Agregar el href
+nuevoEnlace.href  = "nuevo-enlace.html";
+
+//Agregar el texto
+nuevoEnlace.textContent = 'Tienda Virtual';
+
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Agregarlo al documento
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
